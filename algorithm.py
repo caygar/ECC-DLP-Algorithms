e4148fbf1aa8941019b18046e9baf6f2e8c2aad7
caygar <123142790+caygar@users.noreply.github.com>
2023-04-11 11:18:14 -0700

    Commented

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,29 @@
+"""
+crt(a,m): An implementation of the chinese remainder theorem. A mathematical theorem that states that given a system
+          of linear congruences with pairwise relatively prime moduli, there exists a unique solution that can be computed
+          using the product of the moduli and their inverses.
+          
+          Takes in two lists as input, and returns an integer representing the unique solution to the system of linear congruences modulo the product
+          of the moduli.
+          
+naive_bsgs(x1,y1,x2,y2,E,q):  An implementation of the Baby-step Giant-step algorithm, which computes the discrete logarithm of a point
+                              Q (coordinates are given with x2, y2) with respect to a point P (coordinates are given with x1, y1) on the elliptic curve E.
+                              
+                              The algorithm works by first computing a series of 'baby steps' that are multiples of P up to a certain limit (which is the
+                              supgroup_order in our implementation), and then computing a series of 'giant steps' that are multiples of Q down to that same limit.
+                              
+                              If both steps converge, we have found the discrete logarithm. If not, the algorithm repeats until it either finds the logarithm, or exceeds
+                              the search limit. In that case, the function returns None.
+                              
+
+polhig_hellman(x1,y1,x2,y2,E,q):  An implementation of the Polhig Hellman algorithm, which computes the discrete logarithm of a point (h) with respect to another point
+                                  (g, in this case). Firstly it factors the order of the subgroup, then calculates the discrete logarithm mod each prime factor using
+                                  the Baby-step Giant-step algorithm we implemented previously. Lastly, uses our implementation of the Chinese Remainder Theorem to
+                                  combine the solutions for each prime factor and return the overall solution.
+
+"""
+
+
 from elliptic import EllipticCurve
 from math import ceil, sqrt
 from sympy import factorint
